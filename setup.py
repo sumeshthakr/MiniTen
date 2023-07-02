35a4268092672821826ff18feb473a44ff8dd9cb
Sumesh Thakur <sumeshthakur@Sumeshs-MacBook-Air.local>
2023-07-02 13:31:43 -0300

    test

--- setup.py
+++ setup.py
@@ -3,8 +3,18 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 import numpy
+from setuptools.command.test import test as TestCommand
+import numpy
+import sys
+
+class PyTest(TestCommand):
+    def run_tests(self):
+        import pytest
+        errno = pytest.main(["--cov=./", "--cov-report=term-missing"])
+        sys.exit(errno)
 
 setup(
-    ext_modules=cythonize("vec_multi.pyx"),
-    include_dirs=[numpy.get_include()]
+    ext_modules=cythonize("vector_operations.pyx"),
+    include_dirs=[numpy.get_include()],
+    cmdclass={'test': PyTest}
 )
